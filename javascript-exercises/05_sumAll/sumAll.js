438b3991afef3cfe671e9af385339813269a89be
st-izzy <165193432+st-izzy@users.noreply.github.com>
2024-04-02 16:17:12 -0700

    Complete javascript-exercises/05_sumAll

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function( numA, numB ) {
+    if ( ( numA < 0 ) || ( numB < 0 ) || 
+            ( !Number.isInteger(numA) ) || ( !Number.isInteger(numB) ) ) {
+        return "ERROR"
+    }
+    if ( numB < numA ) {
+        let temp = numA
+        numA = numB
+        numB = temp
+    }
 
+    let sum = 0
+    for (let i = numA; i <= numB; i++ ) {
+        sum += i
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
