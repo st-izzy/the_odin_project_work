edcddca352eaa44e62afa276fd79e9321ef1aa73
st-izzy <165193432+st-izzy@users.noreply.github.com>
2024-04-02 12:32:10 -0700

    Complete javascript-exercises/03_reverseString

--- javascript-exercises/03_reverseString/reverseString.js
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let outputString = ""
+    for ( let i= 0; i < inputString.length; i++ ) {
+        outputString += inputString.charAt(inputString.length - i - 1)
+    }
 
+    return outputString
 };
 
 // Do not edit below this line
