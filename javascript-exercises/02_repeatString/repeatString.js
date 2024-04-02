5f9d2fd5afa7048fb5101ec4e19f35102992684b
st-izzy <165193432+st-izzy@users.noreply.github.com>
2024-04-02 12:15:09 -0700

    Complete javascript-exercises/02_repeatstring

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, repeatNumber) {
+    if ( repeatNumber < 0 ) { return "ERROR" } // catch negative numbers
+    let outputString = "" // don't forget the quotes
+    for (let i = 0; i < repeatNumber; i++ ) {
+        outputString += inputString // += is cool
+    }
+    return outputString
 };
 
 // Do not edit below this line
