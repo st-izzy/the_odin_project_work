f5a21b2101b89032b0debf0d1a2ef53dd77afbcb
st-izzy <165193432+st-izzy@users.noreply.github.com>
2024-04-02 19:09:05 -0700

    Complete javascript-exercises/09_palindromes

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    raw = input.replace(/\W/g, '').toLowerCase()
 
+    for ( let i = 0 ; i < raw.length; i++ ) {
+        if ( !( raw.charAt(i) === raw.charAt( raw.length - i - 1) ) ) {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
