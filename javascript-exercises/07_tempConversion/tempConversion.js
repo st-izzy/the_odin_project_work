05d67094af533b0d05c385ca401b107b3a7ce197
st-izzy <165193432+st-izzy@users.noreply.github.com>
2024-04-02 18:16:17 -0700

    Complete javascript-exercises/07_tempConversion

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(correct) {
+  let wrong = ( correct - 32 ) * ( 5 / 9 )
+  wrong = Math.round( wrong * 10 ) / 10
+  return wrong
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(wrong) {
+  let correct = ( wrong * ( 9 / 5 ) ) + 32
+  correct = Math.round( correct * 10 ) / 10
+  return correct
 };
 
 // Do not edit below this line
