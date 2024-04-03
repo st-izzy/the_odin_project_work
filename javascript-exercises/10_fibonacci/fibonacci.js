b3430efca516101bc07fb0c46f71c407a177f780
st-izzy <165193432+st-izzy@users.noreply.github.com>
2024-04-02 19:56:04 -0700

    Complete javascript-exercises/10_fibonacci

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = Number(index)
+    if ( index === 0 ) {
+        return 0
+    } else if ( index === 1 ) {
+        return 1
+    } else if ( index < 0 ) {
+        return "OOPS"
+    }
+    let oldNum = 0
+    let newNum = 1
+    let answer = 0
+    for ( let i = 1; i < index; i++ ) {
+        answer = newNum + oldNum
+        oldNum = newNum
+        newNum = answer
+    }
 
+    return answer
 };
 
 // Do not edit below this line
