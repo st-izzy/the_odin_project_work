ab00a154ca52e7ef1fcc0558e3907165b9645a7e
st-izzy <165193432+st-izzy@users.noreply.github.com>
2024-04-02 18:42:34 -0700

    Complete javascript-exercises/08_calculator

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(numA, numB) {
+  return numA + numB
 };
 
-const subtract = function() {
-	
+const subtract = function(numA, numB) {
+  return numA - numB  
 };
 
-const sum = function() {
-	
+const sum = function(toSum) {
+	let theSum = 0
+  toSum.forEach( (num) => {
+    theSum += num
+  } )
+  return theSum
 };
 
-const multiply = function() {
-
+const multiply = function(toMultiply) {
+  let answer = 1
+  toMultiply.forEach( (multiple) => {
+    answer = answer * multiple
+  } )
+  return answer
 };
 
-const power = function() {
-	
+const power = function(numA, numB) {
+	return numA ** numB
 };
 
-const factorial = function() {
-	
+const factorial = function(integer) {
+  if ( integer === 0 ) { return 1 };
+  let answer = integer
+  for ( let i = 1; i < integer; i++ ) {
+    answer = answer * i
+  }	
+  return answer
 };
 
 // Do not edit below this line
